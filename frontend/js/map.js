11796b23347e6d29894d1b56617fd7bd6d82614e
Daniel <dannysht27@gmail.com>
2023-10-11 22:31:26 +0300

    Fixed image background for PC, added find location button

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,8 +1,36 @@
+let map, marker;
+
 function initMap() {
-  var dumbo = {lat: 40.700802, lng:73.987602};
-  var mapOptions = {
-      center: dumbo,
-      zoom: 10
-  };
-  var googlemap = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+const plovdiv = {lat: 42.1354, lng:24.7453};
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: plovdiv,
+    zoom: 17,
+  });
+}
+
+function showCurrentLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+          var currentLocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+          };
+          
+          if (marker) {
+              marker.setPosition(currentLocation);
+          } else {
+              marker = new google.maps.Marker({
+                  position: currentLocation,
+                  map: map,
+                  title: "Your Location"
+              });
+          }
+          
+          map.setCenter(currentLocation);
+      });
+  } else {
+      alert("Geolocation is not supported by your browser.");
+  }
 }
