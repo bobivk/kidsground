7754cc25f8a8785becb0b890171faae93824392c
bobivk <bobby.kavalov@gmail.com>
2023-11-20 23:44:50 +0200

    menu anchors, delete old files

--- frontend/js/project.js
+++
@@ -1,9 +0,0 @@
-class Project {
-    constructor(id, name, number, description, status) {
-        this.id = id;
-        this.name = name;
-        this.number = number;
-        this.description = description;
-        this.status = status;
-    }
-}
