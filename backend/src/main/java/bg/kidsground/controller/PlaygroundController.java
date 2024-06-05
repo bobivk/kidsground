dd37eed106bd88ba772ddb938067f7ff8aee8662
bobivk <bobby.kavalov@gmail.com>
2024-06-05 17:16:30 +0300

    response entity

--- backend/src/main/java/bg/kidsground/controller/PlaygroundController.java
+++ backend/src/main/java/bg/kidsground/controller/PlaygroundController.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class PlaygroundController {
 
@@ -18,29 +18,42 @@ public class PlaygroundController {
     private PlaygroundService playgroundService;
 
     @PostMapping(path = AppRestEndpoints.V1.Playground.ADD_PLAYGROUND)
-    public void savePlayground(@RequestBody final Playground playground) {
+    public ResponseEntity<Void> savePlayground(@RequestBody final Playground playground) {
         this.playgroundService.savePlayground(playground);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping(path = AppRestEndpoints.V1.Playground.ADD_PLAYGROUND)
-    public ResponseEntity<Playground> updatePlayground(@RequestBody final Playground playground) {
-        return ResponseEntity.ok(this.playgroundService.updatePlayground(playground));
+    public ResponseEntity<Void> updatePlayground(@RequestBody final Playground playground) {
+        this.playgroundService.updatePlayground(playground);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping(path = AppRestEndpoints.V1.Playground.By.ID)
-    public Playground getById(@PathVariable final Long id) {
-        return this.playgroundService.getById(id);
+    public ResponseEntity<Playground> getById(@PathVariable final Long id) {
+        return ResponseEntity.ok(this.playgroundService.getById(id));
+    }
+
+    // provide a list of PlaygroundSummary if this gets too slow due to size
+    @GetMapping(path = AppRestEndpoints.V1.Playground.GET_ALL)
+    public ResponseEntity<List<Playground>> getAll() {
+        return ResponseEntity.ok(this.playgroundService.getAll());
+    }
+
+    @GetMapping(path = AppRestEndpoints.V1.Playground.COUNT)
+    public ResponseEntity<Integer> getCount() {
+        return ResponseEntity.ok(this.playgroundService.getCount());
     }
 
     @DeleteMapping(path = AppRestEndpoints.V1.Playground.By.ID)
-    public Playground deleteByID(@PathVariable final Long id) {
-        return this.playgroundService.deleteById(id);
+    public ResponseEntity<Playground> deleteByID(@PathVariable final Long id) {
+        return ResponseEntity.ok(this.playgroundService.deleteById(id));
     }
 
     @ResponseBody
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String employeeNotFoundHandler(final NoSuchElementException ex) {
+    public String playgroundNotFoundHandler(final NoSuchElementException ex) {
         return ex.getMessage();
     }
 }
--- backend/src/main/java/bg/kidsground/service/PlaygroundService.java
+++ backend/src/main/java/bg/kidsground/service/PlaygroundService.java
@@ -2,12 +2,18 @@ package bg.kidsground.service;
 
 import bg.kidsground.domain.Playground;
 
+import java.util.List;
+
 public interface PlaygroundService {
 
     void savePlayground(final Playground playground);
 
     Playground getById(final Long id);
 
+
+    List<Playground> getAll();
+
+    Integer getCount();
 
     Playground updatePlayground(Playground playground);
 
