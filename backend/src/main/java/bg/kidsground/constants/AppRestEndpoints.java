ed98bef79efa915555b9efb0d3742f15db2a4e87
Borislav Kavalov <borislav.kavalov@blackswan.com>
2024-06-17 18:31:55 +0300

    playgrounds

--- backend/src/main/java/bg/kidsground/constants/AppRestEndpoints.java
+++ backend/src/main/java/bg/kidsground/constants/AppRestEndpoints.java
@@ -4,7 +4,7 @@ public class AppRestEndpoints {
     public class V1 {
         public static final String V1_ROOT = "/v1";
         public class Playground {
-            public static final String PLAYGROUND_ROOT = V1_ROOT + "/playground";
+            public static final String PLAYGROUND_ROOT = V1_ROOT + "/playgrounds";
 
             public static final String ADD_PLAYGROUND = PLAYGROUND_ROOT + "/add";
             public static final String COUNT = PLAYGROUND_ROOT + "/count";
