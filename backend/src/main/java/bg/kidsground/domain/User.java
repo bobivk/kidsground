e857669576bf39fe2984731d6a933d7f9dd0c060
bobivk <bobby.kavalov@gmail.com>
2024-06-23 15:19:29 +0300

    remove @column

--- backend/src/main/java/bg/kidsground/domain/User.java
+++ backend/src/main/java/bg/kidsground/domain/User.java
@@ -1,30 +1,31 @@
 package bg.kidsground.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "users")
+//@Table(name = "users")
 @Data
 public class User {
   @Id
-  @Column(name = "user_id", length = 45)
+  //@Column(name = "user_id", length = 45)
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @Column(name = "username", length = 255)
+  //@Column(name = "username", length = 255)
+  @JsonProperty("username")
   private String username;
 
-  @Column(name = "password", length = 255)
+  //@Column(name = "password", length = 255)
+  @JsonProperty("password")
   private String password;
 
-  @Column(name = "email", length = 255)
+  //@Column(name = "email", length = 255)
+  @JsonProperty("email")
   private String email;
 
   @JsonProperty("role")
@@ -44,7 +45,7 @@ public class User {
 
   @Override
   public String toString() {
-    return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + "]";
+    return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + ", role=" + role + "]";
   }
 
 }
