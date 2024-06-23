34cd2054ae85fbf1cb72dc9f894b6b27671ee309
bobivk <bobby.kavalov@gmail.com>
2024-06-23 15:06:24 +0300

    remove dependency

--- backend/src/main/java/bg/kidsground/config/WebSecurityConfiguration.java
+++ backend/src/main/java/bg/kidsground/config/WebSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.LogoutConf
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,14 +22,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class WebSecurityConfiguration
 {
-
-    @Autowired
-    private UserRepository userRepo;
     @Autowired
     private JWTFilter filter;
-    @Autowired
-    private UserService userService;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -43,6 +38,7 @@ public class WebSecurityConfiguration
                 .formLogin(formLogin -> formLogin.loginPage(AppRestEndpoints.V1.Users.LOGIN).permitAll())
                 .logout(LogoutConfigurer::permitAll)
                 .httpBasic(withDefaults());
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
