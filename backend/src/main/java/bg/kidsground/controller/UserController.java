00322adcf7809f492178252924662d4991eba4d8
Borislav Kavalov <borislav.kavalov@blackswan.com>
2024-06-24 10:49:00 +0300

    cors for controllers

--- backend/src/main/java/bg/kidsground/controller/UserController.java
+++ backend/src/main/java/bg/kidsground/controller/UserController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api/v1/user")
+@CrossOrigin(origins = {"http://localhost:3000", "https://kidsground.bg"})
 public class UserController {
 
   @Autowired
