740d0df0afed09fb01c67560a81f9f8958685da3
danny <dannysht27@gmail.com>
2024-06-26 13:29:21 +0300

    set playgrounds from fetch

--- frontend/src/Components/Common/Map.jsx
+++ frontend/src/Components/Common/Map.jsx
@@ -134,9 +134,8 @@ export const Map = ({onCoordinatesChange}) => {
     const fetchData = async () => {
         setPlaygrounds([{id:1, name: "bla, bla", ageGroup: "three_to_six", coordinates: {lat: 41.6338, lng: 25.3777}}, {id:2, name: "bla, bla2", ageGroup: "three_to_six", coordinates: {lat: 41.6368, lng: 25.3777}}])
         const receivedItems = await fetch("https://kidsground.bg:8009/v1/playgrounds/all")
-        //172.31.26.186
-        // const receivedItemsJSON = await receivedItems.json()
-        // setPlaygrounds(receivedItemsJSON)
+        const receivedItemsJSON = await receivedItems.json()
+        setPlaygrounds(receivedItemsJSON)
     }
 
       useEffect(() => {
