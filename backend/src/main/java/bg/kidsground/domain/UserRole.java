3b7bf7dc7bbe90791839460c10fc22ec50c7e820
Borislav Kavalov <borislav.kavalov@blackswan.com>
2024-07-07 22:36:40 +0300

    add role prefix to enum

--- backend/src/main/java/bg/kidsground/domain/UserRole.java
+++ backend/src/main/java/bg/kidsground/domain/UserRole.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum UserRole {
 
-    ADMIN("admin"),
-    USER("user");
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
 
     public final String value;
     UserRole(final String value){
