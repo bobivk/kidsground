4ede180dce258557f739f512a27bd17aeb13d164
danny <dannysht27@gmail.com>
2024-07-20 11:54:25 +0300

    added table for already existing playgrounds

--- frontend/src/Components/Pages/ProfilePage.jsx
+++ frontend/src/Components/Pages/ProfilePage.jsx
@@ -5,9 +5,10 @@ import '../../static/stylesheets/profilePage.css'
 
 export const ProfilePage = () => {
 
-    const [myPlaygrounds, setMyPlaygrounds] = useState([])
-    const [myComments, setMyComments] = useState([])
-    const [pendingPlaygrounds, setPendingPlaygrounds] = useState([])
+    const [myPlaygrounds, setMyPlaygrounds] = useState([]);
+    const [myComments, setMyComments] = useState([]);
+    const [pendingPlaygrounds, setPendingPlaygrounds] = useState([]);
+    const [existingPlaygrounds, setExistingPlaygrounds] = useState([]);
     const navigate = useNavigate();
 
     const ageGroups = Object.freeze({
@@ -36,7 +37,13 @@ export const ProfilePage = () => {
     // }
 
     const approvePlayground = async (id) => {
-        await fetch(`https://kidsground.bg:8009/v1/playgrounds/${id}/approve`, {
+        await fetch(`https://kidsground.bg:8009/v1/playgrounds/${id}/approve?isApproved=true`, {
+            method: "POST"
+        })
+    }
+
+    const disapprovePlayground = async (id) => {
+        await fetch(`https://kidsground.bg:8009/v1/playgrounds/${id}/approve?isApproved=false`, {
             method: "POST"
         })
     }
@@ -47,11 +54,15 @@ export const ProfilePage = () => {
         })
     }
 
+
     const fetchPendingPlaygrounds = async () => {
         await fetch(`https://kidsground.bg:8009/v1/playgrounds/toApprove`).then(response => response.json()).then((data) => {
             setPendingPlaygrounds(data);  
         })
         
+        await fetch(`https://kidsground.bg:8009/v1/playgrounds/all`).then(response => response.json()).then((data) => {
+            setExistingPlaygrounds(data);  
+        })
     }
 
     useEffect(() => {
@@ -96,7 +107,7 @@ export const ProfilePage = () => {
                             <td>{playground.coordinates.lat}, {playground.coordinates.lng}</td>
                             <td>
                                 <button onClick={() => {approvePlayground(playground.id)}}>✓</button>
-                                <button onClick={() => {deletePlayground(playground.id)}}>✗</button>
+                                <button onClick={() => {disapprovePlayground(playground.id)}}>✗</button>
                             </td>
                         </tr>
                     ))}
@@ -110,6 +121,41 @@ export const ProfilePage = () => {
                 {myComments && myComments.map((comment) => {
                     
                 })}
+                <h2>Съществуващи площадки: </h2>
+                <div className="table-container">
+                    <table>
+                        <tr>
+                            <th>Име</th>
+                            <th>Възрастова група</th>
+                            <th>Местоположение</th>
+                            <th>Сенчести зони</th>
+                            <th>Транспорт</th>
+                            <th>Оградена</th>
+                            <th>Настилка</th>
+                            <th>Катерушки</th>
+                            <th>Други съоръжения</th>
+                            <th>Координати</th>
+                            <th></th>
+                        </tr>
+                    {existingPlaygrounds && existingPlaygrounds.map((playground) => (
+                        <tr>
+                            <td onClick={() => {navigate(`/playground/${playground.id}`)}} key={playground.id} className="playground-link">{playground.name}</td>
+                            <td>{ageGroups[playground.age_group]}</td>
+                            <td>{playground.environment}</td>
+                            <td>{shades[playground.shade_type]}</td>
+                            <td>{playground.transport.join(', ')}</td>
+                            <td>{playground.hasFence ? "Оградена" : "Неоградена"}</td>
+                            <td>{playground.floor_type.join(', ')}</td>
+                            <td>{playground.toys.join(', ')}</td>
+                            <td>{playground.facilities.join(', ')}</td>
+                            <td>{playground.coordinates.lat}, {playground.coordinates.lng}</td>
+                            <td>
+                                <button onClick={() => {deletePlayground(playground.id)}}>✗</button>
+                            </td>
+                        </tr>
+                    ))}
+                    </table>
+                </div>
             </div>
         );
     }
